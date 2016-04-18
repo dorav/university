052efa3e425b9d87188f7f223ba0fa1325007edd
Dor Avraham <dor.a2003@gmail.com>
2016-04-18 22:26:07 +0300

    Added the first version of openu video downloader

---
+++ openu_video_fetcher/Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace openu_video_fetcher
+{
+    class ProgramArgs
+    {
+        [Option('u', "username", Required = true, HelpText = "openu.ac.il/sheilta username.")]
+        public string UserName { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "openu.ac.il/sheilta password.")]
+        public string Password { get; set; }
+
+        [Option('i', "id", Required = true, HelpText = "openu.ac.il/sheilta id.")]
+        public string ID { get; set; }
+
+        [Option('l', "playlist", Required = true, HelpText = "The playlist you wish to download")]
+        public string PlaylistURL { get; set; }
+
+        public DirectoryInfo DownloadDirectory { get; set; }
+        public DirectoryInfo CacheDirectory { get; set; }
+
+        [Option('d', "directory", Required = true, HelpText = "The playlist you wish to download")]
+        public string DownloadDirectoryPath
+        {
+            get
+            {
+                return DownloadDirectory.FullName;
+            }
+            set
+            {
+                if (Directory.Exists(value))
+                    DownloadDirectory = new DirectoryInfo(value);
+                else
+                    CreateDownloadDir(value);
+
+                string cacheDirName = "cache";
+                string cacheDirPath = Path.Combine(DownloadDirectory.FullName, cacheDirName);
+                if (DownloadDirectory != null && Directory.Exists(cacheDirPath))
+                {
+                    CacheDirectory = new DirectoryInfo(cacheDirPath);
+                }
+                else
+                {
+                    try
+                    {
+                        CacheDirectory = DownloadDirectory.CreateSubdirectory(cacheDirName);
+                    }
+                    catch
+                    {
+                        Console.Write("Creating directory \"{0}\"...", value);
+                    }
+                }
+            }
+        }
+
+        private void CreateDownloadDir(string value)
+        {
+            Console.Write("Creating directory \"{0}\"...", value);
+            try
+            {
+                DownloadDirectory = Directory.CreateDirectory(value);
+            }
+            catch
+            {
+                Console.WriteLine(" Failed to create directory. Stopping");
+                DownloadDirectory = null;
+                return;
+            }
+
+            Console.WriteLine("OK");
+        }
+
+        [HelpOption('h', "help")]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+
+    class Program
+    {
+        static string referrer = "https://sso.apps.openu.ac.il/SheiltaPortalLogin?T_PLACE=https://sheilta.apps.openu.ac.il/pls/dmyopt2/sheilta.main";
+
+        static ProgramArgs GetCredentials(string[] args)
+        {
+            ProgramArgs opts = new ProgramArgs();
+
+            if (!CommandLine.Parser.Default.ParseArguments(args, opts) || opts.DownloadDirectory == null)
+            {
+                Console.WriteLine("Problem with given arguments. exiting");
+                return null;
+            }
+        
+            return opts;
+        }
+
+        static CookieContainer cookieJar;
+
+        static string GetContext(string playlistHtml)
+        {
+            var contextDelimiter = new Regex("context-(\\d+)");
+            return contextDelimiter.Match(playlistHtml).Groups[1].Value;
+        }
+
+        static string GetCourseId(string playlistHtml)
+        {
+            var contextDelimiter = new Regex(" course-(\\d+)");
+            return contextDelimiter.Match(playlistHtml).Groups[1].Value;
+        }
+
+        static List<String> GetVideoCodes(string html)
+        {
+            List<String> arr = new List<string>();
+            var tokenToFind = new Regex("id=\"playlist(\\d+)");
+            foreach (Match match in tokenToFind.Matches(html))
+                arr.Add(match.Groups[1].Value);
+            return arr;
+        }
+
+        static void DownloadVideo(VideoData data, UrlFileCache cache)
+        {
+            // TODO: prompt user for each video
+            new VideoDownloader(cache, data).Download();
+        }
+
+        static void Main(string[] args)
+        {
+            var opts = GetCredentials(args);
+            if (opts == null)
+                return;
+
+            cookieJar = new CookieContainer();
+            UrlFileCache cache = new UrlFileCache(opts.CacheDirectory);
+
+            if (login(opts))
+            {
+                var videoUrls = GetVideosUrls(opts, cache);
+                videoUrls.ForEach(x => DownloadVideo(x, cache));
+            }
+            else
+            {
+                Console.WriteLine("Failed to login, check your credentials!");
+            }
+        }
+
+        private static List<VideoData> GetVideosUrls(ProgramArgs opts, UrlFileCache cache)
+        {
+            // This request can't be cached as we need cookies from it
+            var playlistPageReq = (HttpWebRequest)WebRequest.Create(opts.PlaylistURL);
+            playlistPageReq.CookieContainer = cookieJar;
+            using (HttpWebResponse playlistPageResp = (HttpWebResponse)playlistPageReq.GetResponse())
+            {
+                if (playlistPageResp.StatusCode == HttpStatusCode.OK)
+                {
+                    var playlistHtml = new StreamReader(playlistPageResp.GetResponseStream()).ReadToEnd();
+                    return GetVideosUrls(playlistHtml, cache);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to get playlist of course videos");
+                    return new List<VideoData>();
+                }
+            }
+        }
+
+        private static List<VideoData> GetVideosUrls(string playlistHtml, UrlFileCache cache)
+        {
+            List<VideoData> videoUrls = new List<VideoData>();
+
+            var contextId = GetContext(playlistHtml);
+            var courseId = GetCourseId(playlistHtml);
+
+            foreach (var vid in GetVideoCodes(playlistHtml))
+            {
+                VideoData data = new VideoData(contextId, courseId, vid);
+                var vidUrlCacheName = data.formatCacheFileName();
+                var vidIdentifier = cache.GetContent(vidUrlCacheName);
+                if (vidIdentifier == null)
+                {
+                    vidIdentifier = GetVidUrl(data);
+                    cache.PutContent(vidUrlCacheName, vidIdentifier);
+                }
+                if (vidIdentifier == null)
+                    Console.WriteLine("Error when trying to fetch video with id = {0}", vid);
+                else
+                {
+                    data.PlaylistUrl = string.Format("http://api.bynetcdn.com/Redirector/openu/manifest/{0}_mp4/HLS/playlist.m3u8", vidIdentifier);
+                    videoUrls.Add(data);
+                }
+            }
+
+            // This way we get the videos in chronological order
+            videoUrls.Reverse();
+            return videoUrls;
+        }
+
+        private static bool login(ProgramArgs opts)
+        {
+            var initialRequest = (HttpWebRequest)WebRequest.Create("https://sso.apps.openu.ac.il/SheiltaPortalProcess");
+            initialRequest.Method = "POST";
+            initialRequest.ContentType = "application/x-www-form-urlencoded";
+            initialRequest.Referer = referrer;
+            initialRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0";
+            initialRequest.CookieContainer = cookieJar;
+
+            // The login page sets an initial cookie to indicate weather the browser supports cookies
+            var loginPage = (HttpWebRequest)WebRequest.Create(referrer);
+            loginPage.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0";
+            loginPage.CookieContainer = cookieJar;
+            loginPage.GetResponse().Close();
+
+            string credFormat = string.Format("p_user={0}&p_sisma={1}&p_mis_student={2}&T_PLACE={3}", opts.UserName, opts.Password, opts.ID, opts.PlaylistURL);
+            PutData(credFormat, initialRequest);
+            HttpWebResponse response = (HttpWebResponse)initialRequest.GetResponse();
+
+            return response.StatusCode == HttpStatusCode.OK &&
+                   response.Cookies.Count > 1;
+        }
+
+        private static string GetVidUrl(VideoData data)
+        {
+            var specificVidParams = String.Format("action=getplaylist&context={0}&playlistid={1}&course={2}", data.ContextId, data.VidId, data.CourseId);
+            var specificVidReq = (HttpWebRequest)WebRequest.Create("http://opal.openu.ac.il/mod/ouilvideocollection/actions.php");
+            specificVidReq.CookieContainer = cookieJar;
+            specificVidReq.Method = "POST";
+            specificVidReq.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            specificVidReq.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0";
+            PutData(specificVidParams, specificVidReq);
+
+            var response = (HttpWebResponse)specificVidReq.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+                return null;
+            var videoData = ResponseAsJson(response);
+
+            JToken value;
+
+            if (videoData.TryGetValue("media", out value))
+                return value["ar"].ToString();
+            return null;
+        }
+
+        private static JObject ResponseAsJson(HttpWebResponse resp)
+        {
+            var specificVidData = new StreamReader(resp.GetResponseStream()).ReadToEnd();
+            try
+            {
+                return JObject.Parse(specificVidData);
+            }
+            catch {}
+
+            return new JObject();
+        }
+
+        private static void PutData(string data, HttpWebRequest request)
+        {
+            var bytes = new UTF8Encoding().GetBytes(data);
+            request.ContentLength = bytes.Length;
+            request.GetRequestStream().Write(bytes, 0, bytes.Length);
+            request.GetRequestStream().Close();
+        }
+    }
+}
---
+++ openu_video_fetcher/VideoDownloader.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace openu_video_fetcher
+{
+    public class VideoData
+    {
+        public VideoData(string contextId, string courseId, string vid)
+        {
+            ContextId = contextId;
+            CourseId = courseId;
+            VidId = vid;
+        }
+
+        public string ContextId { get; set; }
+        public string CourseId { get; set; }
+        public string PlaylistUrl { get; set; }
+        public string VidId { get; set; }
+
+        public string formatCacheFileName()
+        {
+            return string.Format("{0}-{1}-{2}_addr", ContextId, CourseId, VidId);
+        }
+
+        public string formatPlaylistFile()
+        {
+            return formatCacheFileName() + "_file";
+        }
+
+        private string formatChunksFile(string quality)
+        {
+            return formatCacheFileName() + "_chunks_" + quality;
+        }
+
+        internal string formatChunksFileUrl(string wantedQuality)
+        {
+            return formatChunksFile(wantedQuality) + "_url";
+        }
+
+        internal string formatChunksFileContent(string wantedQuality)
+        {
+            return formatChunksFile(wantedQuality) + ".m3u8";
+        }
+
+        internal string formatFFmpegFile()
+        {
+            return formatCacheFileName() + ".m3u8";
+        }
+
+        internal object formatOutputFile()
+        {
+            return formatCacheFileName() + ".mp4";
+        }
+    }
+
+    public class UrlFileCache
+    {
+        public UrlFileCache(DirectoryInfo input)
+        {
+            parentDir = input;
+        }
+
+        public string GetContent(string playlistName, string subDir = "")
+        {
+            try
+            {
+                return RetrieveCachedFile(playlistName, subDir);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public FileInfo GetFile(string key, string subDir = "")
+        {
+            return new FileInfo(Path.Combine(getDstDir(subDir), key));
+        }
+
+        public void PutBinary(string key, byte[] content, string subDir = "")
+        {
+            try
+            {
+                var dstDir = getDstDir(subDir);
+                if (!Directory.Exists(dstDir))
+                    parentDir.CreateSubdirectory(subDir);
+                File.WriteAllBytes(Path.Combine(dstDir, key), content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        public void PutContent(string key, string content, string subDir = "")
+        {
+            try
+            {
+                var dstDir = getDstDir(subDir);
+                if (!Directory.Exists(dstDir))
+                    parentDir.CreateSubdirectory(subDir);
+                File.WriteAllText(Path.Combine(dstDir, key), content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private string RetrieveCachedFile(string key, string subDir)
+        {
+            var dstDir = getDstDir(subDir);
+            return File.ReadAllText(Path.Combine(dstDir, key));
+        }
+
+        private string getDstDir(string subDir)
+        {
+            return Path.Combine(parentDir.FullName, subDir);
+        }
+
+        DirectoryInfo parentDir;
+
+        internal bool Contains(string key, string subDir = "")
+        {
+            var dstDir = Path.Combine(parentDir.FullName, subDir);
+
+            try
+            {
+                return File.Exists(Path.Combine(dstDir, key));
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+    public class VideoDownloader
+    {
+        UrlFileCache cache;
+        VideoData data;
+        ChunksFile chunks;
+        List<string> chunkNames;
+        string baseUrl;
+
+        string wantedQuality = "40000";
+
+        public VideoDownloader(UrlFileCache bank, VideoData data)
+        {
+            this.cache = bank;
+            this.data = data;
+            durations = new ConcurrentBag<TimeSpan>();
+        }
+
+        string requestPlaylist(VideoData data)
+        {
+            var playlistFile = cache.GetContent(data.formatPlaylistFile(), data.VidId);
+            if (playlistFile != null)
+                return playlistFile;
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(data.PlaylistUrl);
+            req.AllowAutoRedirect = true;
+            var response = (HttpWebResponse) req.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+            playlistFile = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            cache.PutContent(data.formatPlaylistFile(), playlistFile, data.VidId);
+            return playlistFile;
+        }
+
+        public void Download()
+        {
+            string playlistM3U8 = requestPlaylist(data);
+            parsePlaylist(playlistM3U8);
+
+            baseUrl = chunks.Url.Remove(chunks.Url.LastIndexOf("/"));
+
+            new Thread(printProgress).Start();
+
+            Parallel.ForEach(chunkNames, new ParallelOptions { MaxDegreeOfParallelism = 20 }, 
+                chunkName => downloadChunk(chunkName));
+
+            var inputFile = cache.GetFile(data.formatFFmpegFile(), data.VidId);
+
+
+            var p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = Path.Combine("ffmpeg", "ffmpeg.exe");
+            p.StartInfo.Arguments = String.Format("-i \"{0}\" -c copy -bsf:a aac_adtstoasc {1}", inputFile, data.formatOutputFile());
+            p.StartInfo.WorkingDirectory = inputFile.Directory.FullName;
+            p.Start();
+
+            var errors = p.StandardError;
+            var output = p.StandardOutput;
+            Console.WriteLine(errors.ReadToEnd());
+            Console.WriteLine(errors.ReadToEnd());
+            p.WaitForExit();
+        }
+
+        private void printProgress()
+        {
+            Stopwatch totalRuntime = new Stopwatch();
+            totalRuntime.Start();
+            while (total != counter)
+            {
+                TimeSpan overallDuration = new TimeSpan();
+                foreach (var dur in durations)
+                    overallDuration = overallDuration.Add(dur);
+
+                double average = 0;
+                long remainingMilli = 0;
+                if (counter != 0)
+                {
+                    average = ((double)overallDuration.TotalMilliseconds) / counter;
+                    remainingMilli = (long)((double)totalRuntime.ElapsedMilliseconds / counter) * (total - counter);
+                }
+                var remaining = TimeSpan.FromMilliseconds(remainingMilli);
+
+                Console.WriteLine("Progress: remaining - {0}, {1} / {2}, active {3}, avg = {4}", 
+                                   remaining, counter, total, active, average);
+                Thread.Sleep((int)TimeSpan.FromSeconds(1).TotalMilliseconds);
+            }
+        }
+
+        private void parsePlaylist(string playlistM3U8)
+        {
+            chunks = getChunksFile(playlistM3U8);
+            var allChunkNames = parseChunksFile(chunks.Content);
+            chunkNames = new List<string>();
+
+            foreach (var chunkName in allChunkNames)
+            {
+                if (cache.Contains(chunkName, data.VidId))
+                    Console.WriteLine("Skipping '{0}' on {1}, piece exists.", chunkName, data.VidId);
+                else
+                    chunkNames.Add(chunkName);
+            }
+
+            total = chunkNames.Count;
+        }
+
+        private ChunksFile getChunksFile(string playlistM3U8)
+        {
+            ChunksFile chunks = new ChunksFile();
+
+            chunks.Url = cache.GetContent(data.formatChunksFileUrl(wantedQuality), data.VidId);
+            chunks.Content = cache.GetContent(data.formatChunksFileContent(wantedQuality), data.VidId);
+
+            if (chunks.Content == null || chunks.Url == null)
+                chunks = fetchAndCacheChunksFile(playlistM3U8);
+
+            return chunks;
+        }
+
+        int counter = 0;
+        int active = 0;
+        ConcurrentBag<TimeSpan> durations;
+        int total;
+
+        private void downloadChunk(string chunkName)
+        {
+            Stopwatch w = new Stopwatch();
+            w.Start();
+            Interlocked.Increment(ref active);
+
+            int tryNum = 0;
+            bool success = false;
+
+            do
+                try
+                {
+                    Console.WriteLine("Started downloading '{0}' on {1}.", chunkName, data.VidId);
+                    var req = WebRequest.Create(baseUrl + "/" + chunkName);
+                    var resp = req.GetResponse();
+                    var chunkData = new BinaryReader(resp.GetResponseStream()).ReadBytes((int)resp.ContentLength);
+
+                    cache.PutBinary(chunkName, chunkData, data.VidId);
+                    success = true;
+                }
+                catch
+                {
+                    tryNum++;
+                    Console.WriteLine("Failed downloading '{0}' on {1}.", chunkName, data.VidId);
+                }
+            while (tryNum < 3 && success == false);
+
+            if (success)
+                Console.WriteLine("Done downloading '{0}' on {1}.", chunkName, data.VidId);
+            else
+            {
+                
+            }
+
+            Interlocked.Increment(ref counter);
+            Interlocked.Decrement(ref active);
+            w.Stop();
+            durations.Add(w.Elapsed);
+        }
+
+        private List<string> parseChunksFile(string chunksFile)
+        {
+            List<string> chunkNames = new List<string>();
+            var ext = ".ts";
+
+            var chunkMetaFinder = new Regex("#EXTINF:");
+
+            using (StringReader sr = new StringReader(chunksFile))
+            {
+                // This is needed because the resources names inside the chunks file
+                // might not be legal file names, that means ffmpeg conversion won't work.
+                using (StringWriter wr = new StringWriter())
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        wr.WriteLine(line);
+                        if (chunkMetaFinder.IsMatch(line))
+                        {
+                            var rawChunkName = sr.ReadLine();
+                            var chunkFileName = rawChunkName.Remove(rawChunkName.IndexOf(ext)) + ext;
+                            wr.WriteLine(chunkFileName);
+                            chunkNames.Add(chunkFileName);
+                        }
+                    }
+                    wr.Close();
+                    cache.PutContent(data.formatFFmpegFile(), wr.ToString(), data.VidId);
+                }
+            }
+
+            return chunkNames;
+        }
+
+        struct ChunksFile
+        {
+            public string Url { get; set; }
+            public string Content { get; set; }
+        }
+
+        private ChunksFile fetchAndCacheChunksFile(string playlistM3U8)
+        {
+            string chunksUrl = getChunksFileUrl(playlistM3U8);
+            var chunksReq = (HttpWebRequest)WebRequest.Create(chunksUrl);
+            var resp = chunksReq.GetResponse();
+            string chunksFile = new StreamReader(resp.GetResponseStream()).ReadToEnd();
+
+            cache.PutContent(data.formatChunksFileUrl(wantedQuality), chunksUrl, data.VidId);
+            cache.PutContent(data.formatChunksFileContent(wantedQuality), chunksFile, data.VidId);
+
+            return new ChunksFile { Url = chunksUrl, Content = chunksFile };
+        }
+
+        private string getChunksFileUrl(string playlistM3U8)
+        {
+            var qualityTester = new Regex(String.Format(":BANDWIDTH={0}", wantedQuality));
+            var lines = playlistM3U8.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (qualityTester.IsMatch(lines[i]))
+                {
+                    return lines[i + 1];
+                }
+            }
+            return null;
+        }
+    }
+}
